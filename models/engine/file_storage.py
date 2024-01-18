5195a5f39aae01f44e5569aa0125878a7a3fade6
Armolas <armolas06@gmail.com>
2024-01-18 01:35:03 +0100

    AirBnB MySQL

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls == None:
+            return FileStorage.__objects
+        else:
+            obj = {}
+            for key, value in FileStorage.__objects.items():
+                if key.__class__ == cls:
+                    obj.update({key: value})
+            return obj
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +55,6 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+    def delete(self, obj=None):
+        if obj in self.__objects:
+            del(obj)
