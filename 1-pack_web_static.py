4290fe820665eaa49ff7c6d35c631b7997ebeab3
Armolas <armolas06@gmail.com>
2024-02-05 12:11:32 +0100

    1-pack_web_static.py

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""uses fabric to generate an archive of web_static folder"""
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """generates a .tgz archive from the contents of the web_static folder"""
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    tar_name = f"web_static_{time}.tgz"
+    local('mkdir -p /versions')
+    result = local(f'tar /versions/{tar_name} web_static')
+    if result.succeeded:
+        return f"/versions/{tar_name}"
+    else:
+        return None
