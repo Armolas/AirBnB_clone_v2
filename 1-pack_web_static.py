a9e0196bd4e95a51e645554c73cf4e7532b1c23b
Armolas <armolas06@gmail.com>
2024-02-05 16:26:05 +0100

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,8 +8,8 @@ def do_pack():
     """generates a .tgz archive from the contents of the web_static folder"""
     time = datetime.now().strftime("%Y%m%d%H%M%S")
     tar_name = f"web_static_{time}.tgz"
-    local('mkdir -p /versions')
-    result = local(f'tar /versions/{tar_name} web_static')
+    local('mkdir -p versions')
+    result = local(f'tar -cvzf versions/{tar_name} web_static')
     if result.succeeded:
         return f"/versions/{tar_name}"
     else:
