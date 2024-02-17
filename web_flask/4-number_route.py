c43ff9e65ba22f7c7fac0acf0fbbeacd13232ae2
Armolas <armolas06@gmail.com>
2024-02-17 13:39:45 +0100

    web_flask/

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+@app.route("/", strict_slashes=False)
+def hello():
+    """displays 'Hello HBNB' at the root dorectory"""
+    return 'Hello HBNB!'
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """displays "HBNB" at root /hbnb"""
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """displays C followed by the text inputed"""
+    return f"C {escape(text.replace('_', ' '))}"
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+def py_route(text, default='is cool'):
+    """displays python followed by the text or is cool"""
+    return f"Python {escape(text.replace('_', ' '))}"
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def display_number(n):
+    """displays 'n' is a number only if it is"""
+    return f"{n} is a number"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/README.md
@@ -0,0 +1,1 @@
+This is the readme file form my web infrastructure project
