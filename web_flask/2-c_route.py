93438acbeaecfde0205cb176752f42c78337dbfe
Armolas <armolas06@gmail.com>
2024-02-17 13:46:45 +0100

    pycodestyle

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,20 +5,25 @@ from markupsafe import escape
 
 
 app = Flask(__name__)
+
+
 @app.route("/", strict_slashes=False)
 def hello():
     """displays 'Hello HBNB' at the root dorectory"""
     return 'Hello HBNB!'
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """displays "HBNB" at root /hbnb"""
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def c_route(text):
     """displays C followed by the text inputed"""
     return f"C {escape(text.replace('_', ' '))}"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -5,20 +5,25 @@ from markupsafe import escape
 
 
 app = Flask(__name__)
+
+
 @app.route("/", strict_slashes=False)
 def hello():
     """displays 'Hello HBNB' at the root dorectory"""
     return 'Hello HBNB!'
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """displays "HBNB" at root /hbnb"""
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def c_route(text):
     """displays C followed by the text inputed"""
     return f"C {escape(text.replace('_', ' '))}"
+
 
 @app.route("/python/<text>", strict_slashes=False)
 @app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
@@ -26,10 +31,12 @@ def py_route(text, default='is cool'):
     """displays python followed by the text or is cool"""
     return f"Python {escape(text.replace('_', ' '))}"
 
+
 @app.route("/number/<int:n>", strict_slashes=False)
 def display_number(n):
     """displays 'n' is a number only if it is"""
     return f"{n} is a number"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
