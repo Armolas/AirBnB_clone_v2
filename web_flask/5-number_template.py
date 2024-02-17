c886c568ce46826876fc2327e977629af2fada4a
Armolas <armolas06@gmail.com>
2024-02-17 14:11:19 +0100

    5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -41,6 +41,7 @@ def display_number(n):
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
+    """displays an html template"""
     return render_template('5-number.html', n=n)
 
 
